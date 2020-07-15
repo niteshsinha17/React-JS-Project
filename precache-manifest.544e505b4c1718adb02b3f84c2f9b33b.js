c47c49c5347e228b4397e07f7613d95bb7f1c834
Nitesh Sinha <niteshsinha1707@gmail.com>
2020-07-15 12:53:11 +0530

    Updates

---
+++ precache-manifest.544e505b4c1718adb02b3f84c2f9b33b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fd13531f36a0a3c38b9e1f92b20ea6",
+    "url": "/niteshsinha17/React-JS-Project.git/index.html"
+  },
+  {
+    "revision": "65a0802a3043541467b8",
+    "url": "/niteshsinha17/React-JS-Project.git/static/css/2.2c296f59.chunk.css"
+  },
+  {
+    "revision": "b5dfe41db40011a97532",
+    "url": "/niteshsinha17/React-JS-Project.git/static/css/main.db750617.chunk.css"
+  },
+  {
+    "revision": "65a0802a3043541467b8",
+    "url": "/niteshsinha17/React-JS-Project.git/static/js/2.3b7ec04a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/niteshsinha17/React-JS-Project.git/static/js/2.3b7ec04a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5dfe41db40011a97532",
+    "url": "/niteshsinha17/React-JS-Project.git/static/js/main.abf47419.chunk.js"
+  },
+  {
+    "revision": "5a83b4649e7590fea64f",
+    "url": "/niteshsinha17/React-JS-Project.git/static/js/runtime-main.486bb7df.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/niteshsinha17/React-JS-Project.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/niteshsinha17/React-JS-Project.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/niteshsinha17/React-JS-Project.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/niteshsinha17/React-JS-Project.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/niteshsinha17/React-JS-Project.git/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
