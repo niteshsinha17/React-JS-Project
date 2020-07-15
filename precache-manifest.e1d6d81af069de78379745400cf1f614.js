ebcb3e5a0ecf62e50d50c8770959539b5a64ad9a
Nitesh Sinha <niteshsinha1707@gmail.com>
2020-07-15 17:58:46 +0530

    Updates

---
+++ precache-manifest.e1d6d81af069de78379745400cf1f614.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b7f2e29e5f30717d179e1bf57d28b2f",
+    "url": "/React-JS-Project/index.html"
+  },
+  {
+    "revision": "95adc2f23dcb331b8cf3",
+    "url": "/React-JS-Project/static/css/2.c1abbbfc.chunk.css"
+  },
+  {
+    "revision": "0ee2bc2c8a078d63c375",
+    "url": "/React-JS-Project/static/css/main.db750617.chunk.css"
+  },
+  {
+    "revision": "95adc2f23dcb331b8cf3",
+    "url": "/React-JS-Project/static/js/2.3b7ec04a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-JS-Project/static/js/2.3b7ec04a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee2bc2c8a078d63c375",
+    "url": "/React-JS-Project/static/js/main.865dfa77.chunk.js"
+  },
+  {
+    "revision": "c9e7a8c3f16ad0350f6e",
+    "url": "/React-JS-Project/static/js/runtime-main.5d2ca0c8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-JS-Project/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-JS-Project/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-JS-Project/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-JS-Project/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-JS-Project/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.066722286ce20237df181e3ff1a21edd.js"
+  "/React-JS-Project/precache-manifest.e1d6d81af069de78379745400cf1f614.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-JS-Project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
